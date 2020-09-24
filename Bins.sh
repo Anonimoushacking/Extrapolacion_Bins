dcfc83227bba597c54a2a55f92e4f0932d2f06fc
AnonimousHacking <69918140+Anonimoushacking@users.noreply.github.com>
2020-09-24 09:22:56 -0500

    Update Bins.sh

--- Bins.sh
+++ Bins.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 apt install figlet
 clear
-figlet Extrapola 
-figlet Bins 
-figlet -f term Doble Extrapolacion 
-figlet -f term by. Anonimous 
-figlet -f term ==============================
-echo "
-figlet -f term            DISFRUTA 
-figlet -f term ===============================
+figlet -f smslant BIN -c
+figlet -f smslant ReSuci+aTe -c  
+figlet -f term Doble Extrapolacion -c 
+figlet -f term by. ༆𝔸𝐍𝑂Nɪᗰ𝗼𝖀𝘀♕ -c    
+figlet -f term ==================================== -c 
+echo ""
+figlet -f term         DISFRUTA -c 
+figlet -f term ==================================== -c
 echo ""    
-echo "=>Primera extrapolacion, pon los ultimos 4 dijitos de una CC Real "
-read -p " 4 digitos===> " CCR
+echo "Primer extrapolacion, escriba los 4 ultimos digitos de una cc real"
+read -p "==> 4 digitos /> " CCR
 echo ""
-echo "=> Pon dos taretas generadas de un bin cualquiera ponlas sin el cvv y la fecha "
-read -p "Tarjeta generada N°1,=> " TG1
-read -p "Tarjeta generada N°2,=> " TG2
+echo "Escriba dos tarjetas generadas de un bin de 6 digitos sin el cvv y fecha"
+read -p "==> tarjeta generada N° 1, /> " TG1
+read -p "==> tarjeta generada N° 2, /> " TG2
 echo ""
-figlet -f term PRIMERA EXTRAPOLACION
+figlet -f term =============== extrapolacion num. 1 ===============  -c
 echo "" 
-echo "bin extrapolado 1 ${TG1:0:7}x${TG1:8:1}x${TG1:10:1}x$CCR"
-echo "bin extrapolado 2 ${TG2:0:7}x${TG2:8:1}x${TG2:10:1}x$CCR"
+echo "1 bin extrapolado ${TG1:0:7}x${TG1:8:1}x${TG1:10:1}x$CCR"
+echo "2 bin extrapolado ${TG2:0:7}x${TG2:8:1}x${TG2:10:1}x$CCR"
 echo ""
-echo "Ahora genera una tarjeta de cada extrapolado y pon una de cada bin para finalizar la extrapolacion IN LA FECHA NI CVV"
+echo "Nota. copie las dos tarjetas extrapoladas y genere una tarjeta de cada bin en namso-gen y pegue acontinuacion"
 echo ""
-read -p "tarjeta generada N°1 => " TGX1
-read -p "tarjeta generada N°2 => " TGX2
+read -p "==> tarjeta generada N° 1 /> " TGX1
+read -p "==> tarjeta generada N° 2 /> " TGX2
 a1=${TGX1:0:8} 
 b1=${TGX1:9:1}
 c1=${TGX1:10:1}
@@ -40,18 +40,19 @@ let R1=$N1*5
 let R2=$N2*5 
 P=$((R1 + R2))
 echo ""
-figlet -f term SEGUNDA EXTRAPOLACION
+figlet -f term =============== extrapolacion sofia num. 2 ===============  -c
 echo ""
 echo "[#] nuevo Bin" $a1$P"xxxxxx"
 echo "[#] nuevo Bin" $a2$P"xxxxxx"
 echo ""
-figlet smslant Nota 
+figlet  Nota 
 echo ""
-figlet -f term Tu nuevo bin tiene como ip la misma, no cambia
-figlet -f term El codigo postal o direccion no cambian 
+figlet -f term Tu nuevo bin tiene como ip la misma, no cambia -c
+figlet -f term El codigo postal o direccion no cambian -c
 echo ""
-figlet -f term Listo ahora tienes un nuevo bin con lives 
-figlet -f term sin usar checker, tambien puede 
-figlet -f term convertirse en multi bin 
-figlet -f term creditos ༆𝔸𝐍𝑂Nɪᗰ𝗼𝖀𝘀♕  y  Wilder Maldonado 
+figlet -f term Listo ahora tienes un nuevo bin con lives -c
+figlet -f term sin usar checker, tambien puede -c
+figlet -f term convertirse en multi bin -c
+figlet -f term creditos ༆𝔸𝐍𝑂Nɪᗰ𝗼𝖀𝘀♕ y Wilder Maldonado
+echo ""
 exit
